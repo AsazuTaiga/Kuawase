30df94ceac7791f5440135e1ec3c0b9fa76c8010
ASAZU TAIGA <taiga.asazu@icloud.com>
2019-12-30 17:43:52 +0900

    Add databinding settings to qrshow.

--- presentation/src/main/java/com/kuawase/kuawase/screen/qrshow/QRShowViewModel.java
+++ presentation/src/main/java/com/kuawase/kuawase/screen/qrshow/QRShowViewModel.java
@@ -4,6 +4,9 @@ import android.graphics.Bitmap;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.BindingAdapter;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.zxing.BarcodeFormat;
@@ -13,23 +16,36 @@ import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Objects;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class QRShowViewModel extends ViewModel {
     private static final int size = 300;
 
-    void showQrCodeDrawable(@NonNull ImageView imageView, @NonNull String content) {
+    @Nullable
+    private final MutableLiveData<Bitmap> qrImageLiveData = new MutableLiveData<>();
+
+    @BindingAdapter("imageBitmap")
+    public static void loadImageBitmap(ImageView iv, Bitmap bitmap) {
+        iv.setImageBitmap(bitmap);
+    }
+
+    void setQrImageLiveDataFromString(@NonNull String content) {
         Map<EncodeHintType, Object> hints = new EnumMap<>(EncodeHintType.class);
         hints.put(EncodeHintType.CHARACTER_SET, UTF_8);
         BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-        Bitmap bitmap = null;
+        Objects.requireNonNull(qrImageLiveData);
         try {
-            bitmap = barcodeEncoder.encodeBitmap(
-                    content, BarcodeFormat.QR_CODE, size, size, hints);
+            qrImageLiveData.setValue(barcodeEncoder.encodeBitmap(
+                    content, BarcodeFormat.QR_CODE, size, size, hints));
         } catch (WriterException e) {
             e.printStackTrace();
         }
-        imageView.setImageBitmap(bitmap);
+    }
+
+    @Nullable
+    public MutableLiveData<Bitmap> getQrImageLiveData() {
+        return qrImageLiveData;
     }
 }
